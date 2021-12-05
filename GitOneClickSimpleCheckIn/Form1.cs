fb124b4912a4b9187224e58c2d78ce94d3dec5bc
maanoel <manoelvitorbrito@gmail.com>
2021-12-05 17:09:25 -0300

    Ajustando mensagem de aviso no rollback do c¾digo

--- GitOneClickSimpleCheckIn/Form1.cs
+++ GitOneClickSimpleCheckIn/Form1.cs
@@ -61,7 +61,7 @@ namespace GitOneClickSimpleCheckIn
 
     private void button3_Click(object sender, EventArgs e)
     {
-      if(!validadorEntrada.PathDoGitFoiDefinido() || validadorEntrada.ConfirmaRollback()) return;
+      if(!validadorEntrada.PathDoGitFoiDefinido() || !validadorEntrada.ConfirmaRollback()) return;
 
       string gitProjectPath = txtPath.Text;
       string allCommandsForSimpleCheckIn = ComandosGit.Rollback();
